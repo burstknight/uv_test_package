c6701f938520987ac4eac70435a64fce81576ee5
burstknight <burstknight@gmail.com>
2026-03-23 15:30:38 +0800

    feat: Add `math.py`

---
+++ src/uv_test_package/math.py
@@ -0,0 +1,19 @@
+def add(a: float, b: float) -> float:
+    """
+    Add two numbers.
+
+    This function can add two numbers.
+
+    Args:
+        a (float): Give the first number.
+        b (float): Give the second number.
+
+    Returns:
+        float: Gvie the sum.
+
+    Example:
+        >>> add(5, 8)
+        13
+    """
+    return a + b
+# End of add
